cc2a5934b022ceed50e6c1614366520ddca3c265
Parth Patel <parth.b.patel5@gmail.com>
2023-09-04 22:54:39 -0500

    fixed modal

---
+++ src/privacypolicy.js
@@ -0,0 +1,34 @@
+import meet from "./assets/meetanshi 1.png"
+import can from "./assets/cantech 1.png"
+import krupal from "./assets/krupal 1.png"
+import land from "./assets/Landmark  1.png"
+import sna from "./assets/snazzy  1.png"
+
+const styles = {
+    num:{
+        fontSize:40,
+        marginTop:20,
+        color:"#9A9AFF"
+    },
+    numMobile:{
+        fontSize:25,
+        marginTop:20,
+        color:"#9A9AFF"
+    },
+    numDes:{
+        fontSize:12
+    },
+    numDesMobile:{
+        fontSize:10
+    },
+}
+
+function PrivacyPolicy({mobile}){
+    return(
+        <div style={{backgroundColor:"white", width:"100%",display:"flex", flexDirection:"column", fontFamily:"Poppins"}}>
+            <label>Privacy Policy!!!!!!!!!!!!!!!!!!!!!</label>
+        </div>
+    )
+}
+
+export default PrivacyPolicy;
