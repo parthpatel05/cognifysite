4f9dc6432b33658cf7a5032879252f3806bd6aa0
JaysPorfolio <mhpatel2020@gmail.com>
2023-09-07 23:35:43 -0500

    Logo

--- src/purchasemodal.js
+++ src/purchasemodal.js
@@ -210,6 +210,7 @@ function PurchaseModal({ plan, mobile, app }) {
                         {/*    </button>*/}
                         {/*</div>*/}
 
+                        
                     </div>
                 </div>
             }
